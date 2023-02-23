44d13a3e516b3427c05369465fec497692dd3b78
sadumpl <104014604+sadumpl@users.noreply.github.com>
2023-02-24 00:02:44 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 document.getElementById('contacts').insertAdjacentHTML('beforeend', new Date().getFullYear());
+
+for (let i = 1; i < 44; i += 1) {
+	document.getElementById('gallery').insertAdjacentHTML('afterbegin', `<img src='./moments/${i}.jpg' />`);
+}
